6d6b9f7b21dd556f13fb04fbda51b0345fa4a448
aroccon <keroro.90@hotmail.it>
2023-05-30 19:00:32 +0200

    update

--- P1_derivs.m
+++
@@ -1,6 +0,0 @@
-%% derivative function
-%% place in file derivs.m
-function dy = P1_derivs(time,y)
-dy = zeros(2,1); %% initialize dy array and orient as column 
-dy(2) =-y(1); %% dv/dt = -x
-dy(1) = y(2); %% dx/dt = v
